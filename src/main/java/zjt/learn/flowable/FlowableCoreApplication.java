d1a1f50ce19bc5bd165ed692ac366ca7b744014e
zhaojiatao <2322795718@qq.com>
2022-06-03 22:17:38 +0800

    Initial commit

---
+++ src/main/java/zjt/learn/flowable/FlowableCoreApplication.java
@@ -0,0 +1,13 @@
+package zjt.learn.flowable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowableCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowableCoreApplication.class, args);
+    }
+
+}
